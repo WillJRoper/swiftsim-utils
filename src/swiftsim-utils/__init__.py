bd3f8460aaff9f504ee9dd2af07e9dc0b13ce271
wjr21 <wjr21@sussex.ac.uk>
2025-07-23 20:14:43 +0100

    Refined pyproject.toml

--- src/swiftsim-utils/__init__.py
+++ src/swiftsim-utils/__init__.py
@@ -1,5 +1,3 @@
-# If this is the first time we have been imported, we need to set up the
-# package with any missing environment variables etc.
-from swiftsim_utils.config import setup_environment
+""""""
 
-setup_environment()
+from swiftsim_utils._version import __version__
